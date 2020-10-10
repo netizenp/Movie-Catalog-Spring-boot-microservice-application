7400a21be77f24c4c2007d4dd118a636f4e75b9e
Pankaj kumar Verma <vkumarpankaj12@gmail.com>
2020-10-11 01:57:43 +0530

    Created 3 Microservice Spring Boot App

---
+++ MovieCatalogService/src/main/java/com/moviecatalog/ContactUsSystem1Application.java
@@ -0,0 +1,13 @@
+package com.moviecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactUsSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactUsSystem1Application.class, args);
+	}
+
+}
